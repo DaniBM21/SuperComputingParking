ec7d67c281ad3f2e5ac807bb4783de78117436e3
root <root@seax.epsevg.upc.edu>
2019-05-10 18:07:28 +0200

    Canvis a Client

--- Client/systemInfo.sh
+++ Client/systemInfo.sh
@@ -1,31 +1,32 @@
 #! /bin/bash
 
-matricula=$(cat log/accLog.txt  | sed -n '3p')
+matricula=$(cat /root/log/accLog.txt  | sed -n '3p')
 #Comprovamos si existe el archivo info.txt, lo borramos para actualizar-lo.
-[ -e $matricula.txt ] && rm $matricula.txt
+[ -e /root/log/$matricula.txt ] && rm /root/log/$matricula.txt
 
 {
-		echo "##########Información Usuario##########"
-		cat log/accLog.txt
+    echo "##########Información Usuario##########"
+    cat log/accLog.txt
 
     #Información general del sistema.
     echo -e "\n\n\n##########Información General##########"
     echo "Sistema Operativo: $(hostnamectl | grep "Operating System:" | awk '{print substr($0,index($0,$3))}')"
     echo "Kernel: $(hostnamectl | grep "Kernel:" | awk '{print substr($0,index($0,$2))}')"
     echo "Dirección MAC: $(ip ad | grep -m1 "ether" | awk '{print $2}')"
-		echo "Dirección IP: $(hostname -I)"
+    echo "Dirección IP: $(hostname -I)"
 
 
     #Información de la CPU del sistema.
     echo -e "\n\n############Información CPU############"
-    echo "Arquitectura: $(lscpu | grep "Architecture:" | awk '{print $2}')"
+    echo "Arquitectura: $(lscpu | grep "Arquitectura:" | awk '{print $2}')"
+    echo "Modo operación: $(lscpu | grep "operación" | cut -d: -f2)"
     echo "CPU(s): $(lscpu | grep -m1 "CPU(s):" | awk '{print $2}')"
-    echo "Frecuencia: $(lscpu | grep "Model name:" | awk '{print $(NF)}' ORS=' ')"
-    echo "Frecuencia máxima: $(lscpu | grep "CPU max" | awk '{print $4}' | cut -d, -f1)MHz"
-    echo "Frecuencia mínima: $(lscpu | grep "CPU min" | awk '{print $4}' | cut -d, -f1)MHz"
-    echo "Load Average (1 min): $(uptime | awk '{print $8}' | cut -d, -f1,2)"
-    echo "Load Average (5 min): $(uptime | awk '{print $9}' | cut -d, -f1,2)"
-    echo "Load Average (15 min): $(uptime | awk '{print $10}' | cut -d, -f1,2)"
+    echo "Frecuencia: $(lscpu | grep "CPU MHz:" | awk '{print $3}')"
+#   echo "Frecuencia máxima: $(lscpu | grep "CPU max" | awk '{print $4}' | cut -d, -f1)MHz"
+#   echo "Frecuencia mínima: $(lscpu | grep "CPU min" | awk '{print $4}' | cut -d, -f1)MHz"
+    echo "Load Average (1 min): $(uptime | awk '{print $9}' | cut -d, -f1,2)"
+    echo "Load Average (5 min): $(uptime | awk '{print $10}' | cut -d, -f1,2)"
+    echo "Load Average (15 min): $(uptime | awk '{print $11}' | cut -d, -f1,2)"
 
 
     #Información de la memoria principal del sistema.
@@ -46,4 +47,4 @@ matricula=$(cat log/accLog.txt  | sed -n '3p')
     	fi
     done < <(df -H)
 
-} > $matricula.txt
+} > /root/log/$matricula.txt
