eff1b09b9ae0f9305957b3de609b7fa708756184
root <pauolivemontejo@gmail.com>
2019-06-09 07:56:33 +0200

    Cambios

--- Server/servercloud.sh
+++ Server/servercloud.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-while 1:
-	# Si hay algun archivo
-	parking_ID = 0
-	max = 0
-	for x in os.listdir('/root/toExecute'):
-		dir_programa = "/root/toExecute/" + str(x)
-
-		parkings = mysql -u apachitos -p vsp SELECT count(estado_coche) FROM 'plazas', 'coches' WHERE estado_coche = 0 AND plazas.matricula = coches.matricula GROUP BY plazas.parkingID
-	
-		for num in parkings
-			if [parking_ID = 0] then
-				max = num
-				parking_ID = parking_ID + 1
-			else if [num > max]
-				max = num
-				parking_ID = parking_ID + 1
-			
-			fi
-		done
-		 
+while true; do
+	for dir_programa in /var/www/html/dashboard/uploads/*.py; do
+		parkingID=$(mysql -u apachito -pvsp -sNe "SELECT parkingID,count(estado_coche) as c FROM plazas, coches WHERE estado_coche = 1 AND plazas.matricula = coches.matricula GROUP BY plazas.parkingID ORDER BY c DESC LIMIT 1" vilanovasp | awk '{print $1}')
+		#El primer parking tiene IP 10.100.0.10
+		parkingID=$(($parkingID + 8))
 		#Hacemos el SCP
-		dir_parking = "root@10.100.0." + $parking_ID
-		$scp dir_programa root@10.100.0.1:/root/toExecute 
-
-	
+		dir_parking="root@10.100.0."${parkingID}
+		scp $dir_programa $dir_parking:/root/toExecute
+		#rm dir_programa
+		sleep 2
+	done
+	sleep 5
+done
--- Server/subeArchivos.py
+++ Server/subeArchivos.py
@@ -7,17 +7,17 @@ def subeArchivo():
         matricula=x.split("--")[0]
         archivo=x.split("--")[1]
         body = {"matricula":matricula, "estado_coche":"1"}
-		request = requests.post('http://craaxcloud.epsevg.upc.edu:19002/api/actualizar-estat-coche', data = body)
-		request.status_code
+        request = requests.post('http://craaxcloud.epsevg.upc.edu:19002/api/actualizar-estat-coche', data = body)
+        request.status_code
         os.rename('/root/executed/'+x,'/root/executed/'+archivo)
-        archivo = '/root/executed/' + archivo
-        p = subprocess.Popen (["scp", "-i", "id_rsa","-P","19022", archivo, "ptin@10.100.0.1:/var/www/html/dashboard/uploads"])
-		# Esperamos a que termine el envio.
-		sts = os.waitpid(p.pid, 0)
+        archivo = '/root/executed/' + x
+        p = subprocess.Popen (["scp", "-i", "id_rsa", archivo, "ptin@10.100.0.1:/var/www/html/dashboard/downloads"])
+        # Esperamos a que termine el envio.
+        sts = os.waitpid(p.pid, 0)
 
 def main():
     while 1:
         subeArchivo()
-        sleep(10)
+        time.sleep(10)
 
 main()
