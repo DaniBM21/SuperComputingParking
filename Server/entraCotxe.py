918b06c5855de52baaa0ae0f3adfdce492fe69c3
root <pauolivemontejo@gmail.com>
2019-06-25 16:34:21 +0200

    Mejoras robusteza

--- Server/entraCotxe.py
+++ Server/entraCotxe.py
@@ -12,25 +12,36 @@ import shutil
 hostname = socket.gethostname()
 hostname = hostname.strip()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_address = ('0.0.0.0', 120)
-sock.bind(server_address)
+try:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	server_address = ('0.0.0.0', 120)
+	sock.bind(server_address)
+except Exception as e:
+	sys.exit("No s'ha pogut establir la conexió amb la barrera: " + str(e))
 
 while True:
 	print ('\nwaiting to receive message')
-	mat, address = sock.recvfrom(100)
-	mat = bytes(mat).decode("utf-8")
+
+	try:
+		mat, address = sock.recvfrom(100)
+		mat = bytes(mat).decode("utf-8")
+	except Exception as e:
+		sys.exit("No s'ha pogut llegir la matrícula: " + str(e))
+
 	#Rebem la matricula
 	if mat:
 
-		body = {"matricula": mat, "parkingID": hostname[-1]}
-		request = requests.get('http://10.100.0.1:3002/api/comprovar-reserva-cotxe', data= body)
-		reserva = request.json()
-		print(reserva['status'])
+		try:
+			body = {"matricula": mat, "parkingID": hostname[-1]}
+			request = requests.get('http://10.100.0.1:3002/api/comprovar-reserva-cotxe', data= body)
+			reserva = request.json()
+			print(reserva['status'])
+		except Exception as e:
+			sys.exit("No s'ha pogut comrpovar la reserva de la matrícula: " + str(e))
 
 		#Comprovem status de la reserva
 		if(reserva['status'] == 0):
+
 			sPath = "/root/matriculasComprovar/"
 			end = ".txt"
 			dPath = "/root/matriculasValidas/"
@@ -40,21 +51,28 @@ while True:
 			os.rename(sPath, dPath)
 
 			# Preguntem la plaça disponible
-			request2 =  requests.post('http://10.100.0.1:3002/api/introduir-cotxe-parking', data = body)
-			plaza = request2.json()
+			try:
+				request2 =  requests.post('http://10.100.0.1:3002/api/introduir-cotxe-parking', data = body)
+				plaza = request2.json()
 
-			print (body)
-			print(plaza)
-			print(plaza['plazaID'])
-
+				print (body)
+				print(plaza)
+				print(plaza['plazaID'])
+			except Exception as e:
+				sys.exit("ID de la plaça no disponible: " + str(e))
 
 			#Enviem OK a l'entrada del cotxe amb la plaça
-			res = "0,"+str(plaza['plazaID'])
-			sent = sock.sendto(bytes(res.encode("utf-8")), ('10.90.0.18',120))
+			try:
+				res = "0,"+str(plaza['plazaID'])
+				sent = sock.sendto(bytes(res.encode("utf-8")), ('10.90.0.18',120))
+			except Exception as e:
+				sys.exit("Error al enviar OK a la barrera: " + str(e))
 
 		else:
 			#Informem que no pot passar a la barrera
-			res = "1,9"
-			sent = sock.sendto(bytes(res.encode("utf-8")), ('10.90.0.18',120))
-
-			print("El cotxe no té reserva. No pot entrar")
+			try:
+				res = "1,9"
+				sent = sock.sendto(bytes(res.encode("utf-8")), ('10.90.0.18',120))
+				print("El cotxe no té reserva. No pot entrar")
+			except Exception as e:
+				sys.exit("Error al enviar codi de negació de matrícula a la barrera: " +str(e))
