b193a6d22f0a71812f569be4fc9c30def8b17d23
root <pauolivemontejo@gmail.com>
2019-06-26 11:24:53 +0200

    Robustesa sortida

--- Server/surtCotxe.py
+++ Server/surtCotxe.py
@@ -9,41 +9,52 @@ import json
 import os
 import shutil
 
-hostname = socket.gethostname()
-hostname = hostname.strip()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('192.168.43.159', 120)
-sock.bind(server_address)
-
-while True:
-
+#server_address = ('192.168.43.159', 120)
+#sock.bind(server_address)
+while 1:
+	k = k+1
 	try:
 		print ('\nwaiting to receive message')
-		data, address = sock.recvfrom(100)
-		data = bytes(data).decode("utf-8")
+		#data, address = sock.recvfrom(100)
+		#data = bytes(data).decode("utf-8")
+		data = "1"
 		if data:
-			mat = data.split(",")[1]
-			qr = data.split(",")[0]
+			#mat = data.split(",")[1]
+			#qr = data.split(",")[0]
+			mat = "ABC13"
+			qr = "asdfgh123"
+			print(qr)
+			print(mat)
 	except:
 		print("Connexió amb sockets ha fallat")
 
-	if mat and qr:
+	if data and mat and qr:
 		i = 0
-		try:
+		if 1:
 			while i < 3:
 				body = {"matricula": mat, "qr": qr}
+				#request = requests.post('http://craaxcloud.epsevg.upc.edu:19022/api/sortida-cotxe-parking', data = body)
 				request = requests.post('http://10.100.0.1:3002/api/sortida-cotxe-parking', data= body)
 				sortir = request.json()
-				if request.status_code == 200 and str(sortir['status']) == "1":
+				print("Status_code")
+				print(request.status_code)
+				print("Status")
+				print(sortir['status'])
+				if request.status_code == "200" and str(sortir['status']) == "1":
 					n = sortir['status']
 					m = sortir['message']
+					print("Ha entrat on toca")
+					print(n)
+					print(m)
 					res = str(n) + "," + str(m)
 					print(res)
 					sent = sock.sendto(bytes(res.encode("utf-8")), ('192.168.43.76',120))
 					i = 3
-				elif request.status_code == 200:
+				elif request.status_code == "200":
+					print("elif")
 					n = sortir['status']
 					m = "El cotxe pot sortir"
 					res = str(n) + "," + str(m)
@@ -54,24 +65,25 @@ while True:
 					try:
 						j = 0
 						while j < 3:
-							body = ("matricula":mat)
+							body = {"matricula":mat}
 							request = requests.get('http://10.100.0.1:3002/api/comprovar-estat-cotxe', data = body)
 							print("Comprovar estat cotxe")
 							estat = request.json()
-							if request.status_code == 200 and str(estat['status']) == "0":
+							if request.status_code == "200" and str(estat['status']) == "0":
 								if str(estat['estat_coche']) == "2":
 
 									body = {"matricula":mat,"estado_coche":"1"}
 									request = requests.post('http://10.100.0.1:3002/api/actualitzar-estat-cotxe', data = body)
 									act = request.json()
-									if request.status_code == 200 and str(act['status']) == "0":
+									if request.status_code == "200" and str(act['status']) == "0":
 										j = 3
 										dPath = "/root/matriculasValidas/"
 										end = ".txt"
 										dPath = dPath + str(mat) + end
 										os.remove(dPath)
 										print("matricula eliminada del parquing")
-
+									else:
+										j = j+1
 							else:
 								j = j+1
 
@@ -80,8 +92,9 @@ while True:
 						print("Han fallat les consultes d'estat")
 
 				else:
+					print("Ha entrat al else")
 					i = i+1
 
-		except:
+		else:
 			print("Consultes a la api no han funcionat")
 
